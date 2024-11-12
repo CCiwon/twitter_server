8df772f596314904eee854c936ad34d33d530103
강시원 <cwkang2589@gmail.com>
2024-11-12 11:48:24 +0900

    api 테스트

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import tweetsRouter from './router/tweets.js'
+import authRouter from './router/auth.js'
 
 
 const app = express()
 
-app.use(express(json))
+app.use(express.json())
 
 app.use('/tweets',tweetsRouter)
 app.use('/auth',authRouter)
--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-const rotuer = express.Router()
+const router = express.Router()
 
 
 // 회원가입
