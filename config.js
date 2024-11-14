048ba19cf10f2976ecb0d86f1547c23f1cc931ca
강시원 <cwkang2589@gmail.com>
2024-11-14 10:04:43 +0900

    환경변수 추가

---
+++ config.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+
+dotenv.config()// env를 불러옴
+
+function required(key,defaultValue = undefined){
+    const value = process.env[key] || defaultValue // 원래 Env를 못 읽는데 dotenv를 설치하고 config실행하면 process객체를 통해 읽기가능 or key가 들어거나 뒤에것이 들어감
+    if(value==null){
+        throw new Error(`키 ${key}는 undefined!!`)
+    }
+    return value
+}
+
+export const config = {  //외부에서 갖다 쓸수있게 해줌
+    jwt:{
+        secretKey:required('JWT_SECRET'),
+        expiresInSec:parseInt(required('JWT_EXPIRES_SEC',259200))
+    },
+    bcrypt:{
+        saltRounds: parseInt(required('BCRYPT_SALT_ROUNDS',10))
+    },
+    host:{
+        port:parseInt(required('HOST_PORT',8080))
+    }
+
+} //한번 코드 흐름을 다시 볼 필요가있음
